0776657d6299e8a6379c75b280ced43fc993833e
Samuel <61188554+samibanks01@users.noreply.github.com>
2024-11-06 00:26:12 -0900

    Create main.py
    Added solution

---
+++ main.py
@@ -0,0 +1,8 @@
+foodChoice = input("Name a food:")
+plantChoice = input("Name a type of plant:")
+typeOfCook = input("Name a method of cooking:")        
+descFood = input("What word describes burned food?")
+favItem = input("Name a DIY item:")
+
+print("MENU")
+print(typeOfCook, "&", foodChoice, "with", descFood, plantChoice, "on a bed of", favItem)
